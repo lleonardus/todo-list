9c0f00397b846e04f37c0e468574fd0cc887026b
leonardus <leonardus.wizard@gmail.com>
2022-11-24 18:40:44 -0300

    Criando TaskFactory para auxiliar nos testes

---
+++ src/test/java/com/leonardus/todolist/factory/TaskFactory.java
@@ -0,0 +1,35 @@
+package com.leonardus.todolist.factory;
+
+import com.leonardus.todolist.dto.TaskDTO;
+import com.leonardus.todolist.entities.Task;
+import com.leonardus.todolist.entities.enums.TaskStatus;
+
+import java.time.LocalDateTime;
+import java.util.Date;
+
+public class TaskFactory {
+
+    public static Task createTask(){
+        return Task.builder()
+                .id(1L)
+                .title("title")
+                .description("description")
+                .deadLine(new Date())
+                .status(TaskStatus.TO_DO)
+                .createdAt(LocalDateTime.now())
+                .updatedAt(LocalDateTime.now())
+                .build();
+    }
+
+    public static TaskDTO createTaskDTO(){
+        return TaskDTO.builder()
+                .id(1L)
+                .title("title")
+                .description("description")
+                .deadLine(new Date())
+                .status(TaskStatus.TO_DO)
+                .createdAt(LocalDateTime.now())
+                .updatedAt(LocalDateTime.now())
+                .build();
+    }
+}
